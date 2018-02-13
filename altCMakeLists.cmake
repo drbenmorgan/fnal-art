0b0aa2f5cfcd414d50f35ef1e95d910944bae3db
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-02-13 15:35:24 +0000

    Initial altCMake structure
    Create toplevel CMake script with non-UPS tools and find_package.
    Build and lowest level Art library.

---
+++ altCMakeLists.cmake
@@ -0,0 +1,56 @@
+# - Toplevel CMake script for fnal::art
+cmake_minimum_required(VERSION 3.3)
+project(art VERSION 2.10.1)
+
+# - Cetbuildtools, version2
+find_package(cetbuildtools2 0.1.0 REQUIRED)
+set(CMAKE_MODULE_PATH ${cetbuildtools2_MODULE_PATH})
+set(CET_COMPILER_CXX_STANDARD_MINIMUM 14)
+include(CetInstallDirs)
+include(CetCMakeSettings)
+include(CetCompilerSettings)
+
+# Our modules
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/Modules/AltCMake)
+
+#-----------------------------------------------------------------------
+# Dependencies
+find_package(canvas_root_io 1.4.2 REQUIRED)
+find_package(canvas 1.0.6 REQUIRED)
+find_package(messagefacility REQUIRED)
+find_package(fhiclcpp REQUIRED)
+find_package(cetlib REQUIRED)
+find_package(cetlib_except 1.1.0 REQUIRED)
+find_package(CLHEP REQUIRED)
+find_package(SQLite REQUIRED)
+#find_ups_product(range)
+find_package(Boost REQUIRED)
+find_package(ROOT REQUIRED)
+
+# find_package(CPPUNIT REQUIRED ... for testing ...)
+
+# macros for art_dictionary and simple_plugin
+#include(ArtDictionary)
+#include(BuildPlugins)
+
+#if (NOT APPLE)
+#  set(RT -lrt)
+#endif()
+
+# Plugin skeleton generators for cetskelgen.
+#add_subdirectory(perllib)
+
+# tools
+#add_subdirectory(tools)
+
+# source
+add_subdirectory(art)
+
+# ups - table and config files
+#add_subdirectory(ups)
+
+# CMake modules
+#add_subdirectory(Modules)
+
+# packaging utility
+#include(UseCPack)
---
+++ art/Version/altCMakeLists.cmake
@@ -0,0 +1,16 @@
+configure_file(GetReleaseVersion.cc.in GetReleaseVersion.cc @ONLY)
+
+add_library(art_Version SHARED GetReleaseVersion.h ${CMAKE_CURRENT_BINARY_DIR}/GetReleaseVersion.cc)
+target_include_directories(art_Version PUBLIC
+  $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}>
+  $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
+  )
+
+install(TARGETS art_Version
+  EXPORT ${PROJECT_NAME}Targets
+  DESTINATION ${CMAKE_INSTALL_LIBDIR})
+
+install(FILES GetReleaseVersion.h
+  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${PROJECT_NAME}/Version
+  )
+
